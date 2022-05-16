76666ba7914f9407ae17e6984eacf0e28e4a5b1c
Ayushman <ayushsg0412@gmail.com>
2022-05-16 20:48:04 +0530

    Init commit

---
+++ src/models/user.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain password')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    mobilePhone: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (validator.isMobilePhone(value)) {
+                throw new Error('Mobile phone number must be 10 digits')
+            }
+        }
+    },
+})
+
+module.exports = mongoose.models.User || mongoose.model('User', User)
